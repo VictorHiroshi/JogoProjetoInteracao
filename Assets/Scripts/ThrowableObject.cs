da88172911750de2b9bf2bcc8556321f5b8b5e5c
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-05-28 17:19:10 -0300

    Added shooting logic to throwable objects

--- Assets/Scripts/ThrowableObject.cs
+++ Assets/Scripts/ThrowableObject.cs
@@ -4,22 +4,50 @@ using UnityEngine;
 
 public class ThrowableObject : MonoBehaviour {
 
+	public float maxStretch = 2.0f;
+
+	private GameObject slingShot;
 	private Rigidbody2D m_rigidBody;
+	private SpringJoint2D m_springJoint;
 	private CameraController m_Camera;
+	private Ray rayToMouse;
+	private Vector2 prevVelocity;
 	private bool isClicked;
+	private bool launching;
+	private float sqrMaxStretch;
 
-	void Awake () {
+	void Awake () 
+	{
+		slingShot = GameObject.FindWithTag ("SlingShot");
 		m_rigidBody = gameObject.GetComponent <Rigidbody2D> ();
-
+		m_springJoint = gameObject.GetComponent <SpringJoint2D> ();
 		GetCameraController ();
-
 		isClicked = false;
+		launching = false;
 	}
 
-	void Update () {
+	void Start()
+	{
+		rayToMouse = new Ray (slingShot.transform.position, Vector3.zero);
+		sqrMaxStretch = Mathf.Pow (maxStretch, 2);
+	}
+
+	void Update () 
+	{
 		if(isClicked)
 		{
-			
+			Dragging ();
+		}
+
+		if(launching)
+		{
+			if(prevVelocity.sqrMagnitude > m_rigidBody.velocity.sqrMagnitude)
+			{
+				m_springJoint.enabled = false;
+				m_rigidBody.velocity = prevVelocity;
+				launching = false;
+			}
+			prevVelocity = m_rigidBody.velocity;
 		}
 	}
 
@@ -34,6 +62,7 @@ public class ThrowableObject : MonoBehaviour {
 	{
 		m_rigidBody.isKinematic = false;
 		isClicked = false;
+		launching = true;
 		m_Camera.canMove = true;
 	}
 
@@ -49,5 +78,20 @@ public class ThrowableObject : MonoBehaviour {
 		{
 			Debug.LogError ("Unnable to find CameraController");
 		}
+	}
+
+	private void Dragging ()
+	{
+		Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Vector2 slingToMouse = mouseWorldPoint - slingShot.transform.position;
+
+		if(slingToMouse.sqrMagnitude > sqrMaxStretch)
+		{
+			rayToMouse.direction = slingToMouse;
+			mouseWorldPoint = rayToMouse.GetPoint (maxStretch);
+		}
+
+		mouseWorldPoint.z = 0;
+		m_rigidBody.position = mouseWorldPoint;
 	}
 }
