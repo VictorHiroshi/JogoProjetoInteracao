d9964e222b2787da291cc39aacf964dd3fbdee79
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-06-14 20:10:23 -0300

    creates randomic trash list

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
 	public GameObject[] trashObjects;
+	public int trashListSize = 10;
 	public Transform spawnPoint;
 	public GameObject slingShot;
 
 	private int TrashIndex;
 	private bool toInstantiateTrash;
 	private int points;
+	private GameObject[] trashList;
 
 	void Awake () {
 		TrashIndex = 0;
@@ -33,12 +35,15 @@ public class GameManager : MonoBehaviour {
 			Debug.LogError ("No slingshot informed!");
 		}
 
+		trashList = new GameObject[trashListSize];
+		GenerateRandomicTrashList ();
+
 		Debug.Log ("Points: " + points);
 	}
 
 	void Update ()
 	{
-		if(TrashIndex == trashObjects.Length)
+		if(TrashIndex == trashListSize)
 		{
 			GameOver ();
 		}
@@ -68,7 +73,7 @@ public class GameManager : MonoBehaviour {
 
 	private void InstantiateTrash ()
 	{
-		GameObject toInstantiate = trashObjects [TrashIndex];
+		GameObject toInstantiate = trashList [TrashIndex];
 		GameObject instance = Instantiate (toInstantiate, spawnPoint.position, spawnPoint.rotation);
 		SpringJoint2D springJoint = instance.GetComponent <SpringJoint2D> ();
 		springJoint.connectedBody = slingShot.GetComponent <Rigidbody2D> ();
@@ -76,6 +81,15 @@ public class GameManager : MonoBehaviour {
 
 	private void GameOver()
 	{
-		Debug.Log ("Game over!");
+		Debug.Log ("Game over! Total points: " + points);
+	}
+
+	private void GenerateRandomicTrashList ()
+	{
+		Debug.Log ("Generate random: " + trashObjects.Length);
+		for(int i=0; i< trashListSize; i++)
+		{
+			trashList[i] = trashObjects [Random.Range (0, trashObjects.Length)];
+		}
 	}
 }
