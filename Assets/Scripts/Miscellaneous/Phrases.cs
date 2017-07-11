3fe5667b4a2828792b3f2acc9c7441bbc323645f
VictorHiroshi <victor_hiroshi@hotmail.com>
2017-07-11 16:55:01 -0300

    adjustments

--- Assets/Scripts/Miscellaneous/Phrases.cs
+++ Assets/Scripts/Miscellaneous/Phrases.cs
@@ -4,5 +4,5 @@ public class Phrases
 {
 	public static string[] trashOnTheGroundMessage = {"Pare e pense, para onde vai o meu lixo?", "Dê um destino mais adequado à esse material.", "Pare e pense, o que é lixo?"};
 	public static string[] wrongTrashCanMessage = {"Não foi dessa vez!", "Lixeira errada!"};
-	public static string[] gameOverMessage = {"Fim de jogo!"};
+	public static string[] gameOverMessage = {"Fim de jogo! Vamos praticar no dia-a-dia?"};
 }
